eb5831fc503c732d073e81d1b218aadc4e2ced15
Nicola <nyc.zyp@gmail.com>
2023-07-06 22:03:12 +0100

    Exercise A: Variables - Part 2

--- exercise/a_variables/_myAnswers/variables/src/main.rs
+++ exercise/a_variables/_myAnswers/variables/src/main.rs
@@ -1,6 +1,10 @@
-fn main() {
-    let missiles = 8;
-    let ready = 2;
+const STARTING_MISSILES : i32 = 8;
+const READY_AMOUNT : i32 = 2;
 
+fn main() {
+    let (mut missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
+    println!("Firing {} of my {} missiles...", ready, missiles);
+
+    missiles = missiles - ready;
     println!("Firing {} of my {} missiles...", ready, missiles);
 }
